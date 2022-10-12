21ab29d45d745d41a64bfcd96a6ee360fb38f40d
Marut Pandya <pandyamarut@gmail.com>
2022-10-12 00:00:59 -0500

    Fixes the button to open external link in new window on click (#5)
    * Fixes the button to open external link in new window on click
    * Add the props from parent component
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- ui/src/Components/links.js
+++ ui/src/Components/links.js
@@ -3,25 +3,28 @@ import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import {GitHub} from '@mui/icons-material/';
 
-export default function Links() {
-    return (
-        <Stack direction="row" spacing={1} mt={1}>
-            <Chip icon={<GitHub />}
+export default function Links(props) {
+    let {ddClient} = props
+    const handleSSGithub = () => handleClick("https://github.com/deepfence/SecretScanner")
+    const handleTMGithub = () => handleClick("https://github.com/deepfence/ThreatMapper")
+
+    const handleClick = (url) => {
+        { ddClient.host.openExternal(url) };
+    }
+     return (
+         <Stack direction="row" spacing={1} mt={1}>
+             <Chip icon={<GitHub />}
                 label="deepfence/SecretScanner"
                 variant="outlined"
-                component="a"
-                href="https://github.com/deepfence/SecretScanner"
-                target="_blank"
+                onClick={handleSSGithub}
                 clickable
-            />
-            <Chip icon={<GitHub />}
+             />
+             <Chip icon={<GitHub />}
                 label="deepfence/ThreatMapper"
                 variant="outlined"
-                component="a"
-                href="https://github.com/deepfence/ThreatMapper"
-                target="_top"
+                onClick={handleTMGithub}
                 clickable
-            />
-        </Stack>
+             />
+         </Stack>
     );
 }
