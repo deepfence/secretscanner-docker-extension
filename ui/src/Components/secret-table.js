a4df0ce49e145c4e2a0a05fa198981cdd9b96e48
Le-Roi <lc.dzign@gmail.com>
2022-10-12 15:56:53 +0200

    Add feedback states to scanning process

--- ui/src/Components/secret-table.js
+++ ui/src/Components/secret-table.js
@@ -1,5 +1,8 @@
 import * as React from 'react';
+import { Pill } from './pill';
 import { styled } from '@mui/material/styles';
+import { Box } from '@mui/system';
+import { Typography } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -7,7 +10,9 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Pill } from './pill';
+import PrivacyTipTwoToneIcon from '@mui/icons-material/PrivacyTipTwoTone';
+import SafetyCheckTwoToneIcon from '@mui/icons-material/SafetyCheckTwoTone';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -22,7 +27,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
     '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,  
+        backgroundColor: theme.palette.action.hover,
     },
     // hide last border
     '&:last-child td, &:last-child th': {
@@ -30,12 +35,24 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     },
 }));
 
-
 export default function SecretTable(props) {
-    const { rows, handleClickOpen } = props
+
+    const { loadingRows, rows, handleClickOpen } = props
+
+    const styles = {
+        landingBox: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: 'calc(100vh - 25em)'
+        }
+    }
+
     return (
         <TableContainer component={Paper}>
-            <Table stickyHeader sx={{ minWidth: 700 }} aria-label="customized table">
+            <Table stickyHeader sx={{ minWidth: 700, opacity: rows ? '100%' : '10%' }} aria-label="customized table">
                 <TableHead>
                     <TableRow>
                         <StyledTableCell>ID</StyledTableCell>
@@ -48,11 +65,12 @@ export default function SecretTable(props) {
                         <StyledTableCell align="left">Signature to Match</StyledTableCell>
                     </TableRow>
                 </TableHead>
+
                 <TableBody>
-                    {rows.map((row) => (
+                    {rows && rows.map((row) => (
                         <StyledTableRow key={row.name} onClick={() => handleClickOpen(row)} sx={{
                             cursor: 'pointer',
-                          }}>
+                        }}>
                             <StyledTableCell component="th" scope="row" >
                                 <div style={{ width: '120px', display: 'block', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
                                     {row['Image Layer ID']}
@@ -80,6 +98,42 @@ export default function SecretTable(props) {
                     ))}
                 </TableBody>
             </Table>
+
+            {loadingRows &&
+                <Box sx={styles.landingBox}>
+                    <CircularProgress />
+                    <Typography variant="body1" color="text.secondary" sx={{ marginTop: '10px' }}>
+                        Image scan in progress...
+                    </Typography>
+                </Box>
+            }
+
+            {rows?.length === 0 &&
+                <Box sx={styles.landingBox}>
+                    <PrivacyTipTwoToneIcon color='primary' sx={{ fontSize: '100px' }} />
+                    <Typography variant="h3" color="text.secondary" sx={{ mt: 0 }}>
+                        No results found
+                    </Typography>
+
+                    <Typography variant="body1" color="text.secondary" sx={{ mt: 0 }}>
+                        Please retry your scan or select a new image from the dropdown.
+                    </Typography>
+                </Box>
+            }
+
+            {!rows && !loadingRows &&
+                <Box sx={styles.landingBox}>
+                    <SafetyCheckTwoToneIcon color='primary' sx={{ fontSize: '100px' }} />
+                    <Typography variant="h3" color="text.secondary" sx={{ mt: 0 }}>
+                        Start a new scan
+                    </Typography>
+
+                    <Typography variant="body1" color="text.secondary" sx={{ mt: 0 }}>
+                        Select the image from the dropdown and scan for secrets.
+                    </Typography>
+                </Box>
+            }
+
         </TableContainer>
     );
 }
